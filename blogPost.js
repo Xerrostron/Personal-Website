ab786c584a682ada70200bb3f7fcf505994f7ad1
Sammy Martinez <asmartinez01@wm.edu>
2025-01-18 21:12:09 -0500

    Button behavior expanded and content written for about and browse buttons.

---
+++ blogPost.js
@@ -0,0 +1,19 @@
+//div - this is all that a blog post contains!
+//main
+    //h1
+    //p's
+    //br after every p
+
+const blogPosts =[{
+    paragraphs:["Hello my friend!", "Awesome stuff you have here!"],
+    title:"A warm welcome!",
+    author:"Sammy Martinez"
+},{
+    paragraphs:["Everybody gets one."],
+    title:"Spider-Man",
+    author:"Jymothy Greene"
+}];
+
+
+
+
--- navButtons.js
+++ navButtons.js
@@ -27,19 +27,67 @@ function loadAboutPage()
     const contentDiv = document.querySelector(".blogPost");
     contentDiv.innerHTML =
     `
-    <h1>sup</h1>
-    <p>About Page</p>
+    <main>
+    <h1>What Is This Blog About?</h1>
+    <p>This blog is used to share ideas and opinions about video games. The blog seeks to
+    bring forth thoughtful ideas about game design, user experience, and passionate kinship in respect to the Gaming industry!</p>
+    <br>
+    <p> For business inquiries, contact me! </p>
+    </main>
     `;
 }
 function loadBrowsePage()
 {
+    //NOTE: innterHTML does NOT ACTIVATE A SCRIPT!
     const contentDiv = document.querySelector(".blogPost");
     contentDiv.innerHTML = 
     `
-    <h1>sup</h1>
-    <p>Browse Page</p>
+    <main>
+    <h1>Try Some Other Articles!</h1>
+    </main>
     `;
+    //loadArticleTitlesRevised(); this method might not work because of scope with contentDiv
+    const mainBlock = contentDiv.querySelector("main");
 
+    blogPosts.forEach(post => {
+        const button = document.createElement("button"); 
+        button.textContent = post.title; 
+        button.setAttribute("class", "blog-title");
+        mainBlock.appendChild(button); 
+        mainBlock.appendChild(document.createElement("br"));
+    });
+
+}
+const blogPosts =[{
+    paragraphs:["Hello my friend!", "Awesome stuff you have here!"],
+    title:"A warm welcome!",
+    author:"Sammy Martinez"
+},{
+    paragraphs:["Everybody gets one."],
+    title:"Spider-Man",
+    author:"Jymothy Greene"
+}];
+function loadArticleTitlesNaive()
+{
+    let titleArray = [];
+    blogPosts.forEach(post => {
+        titleArray.push(post.title); //post is a placeholder variable representing each object in blogPosts
+    });
+    for(let i = 0; i < titleArray.length; ++i)
+    {
+        console.log(titleArray[i]);
+    }
+}
+function loadArticleTitlesRevised()
+{
+    const mainBlock = contentDiv.querySelector("main");
+
+    // Loop through the blog posts and create <p> elements for each title
+    blogPosts.forEach(post => {
+        const paragraph = document.createElement("p");
+        paragraph.textContent = post.title; // Set the title as the text content
+        mainBlock.appendChild(paragraph); // Append the <p> element to the <main> block
+    });
 }
 
 
